59f29edc3159ed1fa48c236e14e99030715796d3
Coding_Sup <juseop0915@gmail.com>
2024-07-10 13:48:32 +0000

    Commit 5

--- springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -8,5 +8,7 @@ public class SpringmvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
+
+		//요청매핑-API예시 부터 보면 됩니당. 그 전까지 한거는 그냥 @GetMapping쓸수있는 방법들 종류 알아봤음.
 	}
 }
