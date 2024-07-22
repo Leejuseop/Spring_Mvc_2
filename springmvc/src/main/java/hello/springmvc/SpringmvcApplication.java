dd874ccdb3e618629d27fa1e7e1adb180d1b82de
Coding_Sup <juseop0915@gmail.com>
2024-07-22 13:31:56 +0000

    commit 10

--- springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -9,6 +9,7 @@ public class SpringmvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 
-		//요청매핑-API예시 부터 보면 됩니당. 그 전까지 한거는 그냥 @GetMapping쓸수있는 방법들 종류 알아봤음.
+		//요청파라미터를 받는 방법 - @RequestParam, @ModelAttribute.       Http메시지 바디를 읽어 오는 방법 - @RequestBodyㄴ
+		//Http요청 메시지 - JSON부터 보면됨 그니까 지금 뭐하고 있는거냐면 데이터를 받아오는 방법을 배우는중.
 	}
 }
